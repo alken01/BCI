a21f2b47395709f53d1a9d9637d00c09e111362b
Alken Rrokaj <rrokajalken@gmail.com>
2023-03-20 17:33:02 +0100

    added files

---
+++ BCI/BCI/BCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCIApp.swift
+//  BCI
+//
+//  Created by Alken Rrokaj on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
